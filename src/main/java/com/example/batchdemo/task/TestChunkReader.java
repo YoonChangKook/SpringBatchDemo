76a0b3383b4d7b7ee4398aef017817771bc16406
YoonchangKook <yoonchang.kook@nts-corp.com>
2018-12-13 21:36:46 +0900

    Chunk가 StepExecution 대신 Queue를 가지고 있도록 변경

--- src/main/java/com/example/batchdemo/task/TestChunkReader.java
+++ src/main/java/com/example/batchdemo/task/TestChunkReader.java
@@ -11,21 +11,20 @@ import org.springframework.batch.item.ItemReader;
 public class TestChunkReader implements ItemReader<String> {
 	private static final Logger logger = LoggerFactory.getLogger(TestChunkReader.class);
 
-	private StepExecution stepExecution;
+	private Queue<String> datas;
 
 	@BeforeStep
 	public void beforeStep(StepExecution stepExecution) {
 		logger.info("Chunk Step Started");
-		this.stepExecution = stepExecution;
+		this.datas = (Queue<String>)stepExecution.getJobExecution()
+			.getExecutionContext()
+			.get("datas");
 	}
 
 	@Override
 	public String read() {
 		logger.info("Chunk Read");
-		Queue<String> datas = (Queue<String>)this.stepExecution.getJobExecution()
-			.getExecutionContext()
-			.get("datas");
 
-		return datas.poll();
+		return this.datas.poll();
 	}
 }
